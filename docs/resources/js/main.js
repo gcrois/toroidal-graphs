7a11acccac4e4c04034f85d809e092a81e6d77ac
Gregory Croisdale <gtcroisdale1@gmail.com>
2021-04-07 13:40:18 -0400

    content update

--- docs/resources/js/main.js
+++ docs/resources/js/main.js
@@ -19,6 +19,18 @@ function main() {
         },
     },
     );
+    tippy('.tor', {
+        content: 'A graph $G$ which can be drawn on a torus without any intersecting edges',
+        allowHTML: true,
+        placement: "bottom",
+        onShown(instance) {
+            if (!rendered.has(instance.id)) {
+                MathJax.typeset($("#" + instance.popper.id));
+                rendered.add(instance.id);
+            }
+        },
+    },
+    );
     tippy('.kurT', {
         content: 'A graph $G$ is planar <i>if and only if</i> it does not contain any subgraphs that are a subdivision of $K_5$ or $K_{3,3}$',
         allowHTML: true,
